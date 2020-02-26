90329c597c71b2c48809253a8ccb928a4a3b54e1
sara pahlevani <sara.pahlevani@outlook.com>
2020-02-26 18:47:33 +0330

    Add Menu Controller and Dto

---
+++ RealEstateAgency.Dtos/ModelDtos/RBAC/MenuDto.cs
@@ -0,0 +1,52 @@
+﻿using RealEstateAgency.DAL.DtoContracts;
+using RealEstateAgency.DAL.Models;
+
+namespace RealEstateAgency.Dtos.ModelDtos.RBAC
+{
+    public class MenuDto 
+    {
+        
+        public  int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsPanelPage { get; set; }
+        public int? ParentId { get; set; }
+        public string PluginName { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+
+        //public override Menu Create() => new Menu
+        //{
+        //    Id = Id,
+        //    PluginName = PluginName,
+        //    Name = Name,
+        //    IsPanelPage = IsPanelPage,
+        //    ControllerName  = ControllerName,
+        //    ParentId = ParentId,
+        //    ActionName = ActionName
+        //};
+        //public override IModelDto<Menu> From(Menu entity)
+        //{
+
+        //    Id = entity.Id;
+        //    PluginName = entity.PluginName;
+        //    Name = entity.Name;
+        //    IsPanelPage = entity.IsPanelPage;
+        //    ControllerName  = entity.ControllerName ;
+        //    ParentId  = entity.ParentId;
+        //    ActionName = entity.ActionName;
+            
+        //    return this;
+        //}
+
+        //public override Menu Update() => new Menu
+        //{
+        //    Id = Id,
+        //    PluginName = PluginName,
+        //    Name = Name,
+        //    IsPanelPage = IsPanelPage,
+        //    ControllerName = ControllerName,
+        //    ParentId = ParentId,
+        //    ActionName = ActionName
+        //};
+    }
+}
---
+++ RealEstateAgency/Controllers/RBAC/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using RealEstateAgency.Controllers.Contracts;
+using RealEstateAgency.DAL.Models;
+using RealEstateAgency.Dtos.ModelDtos.RBAC;
+using RealEstateAgency.Implementations.ApiImplementations.Services.Contracts;
+using System;
+using System.Linq;
+
+namespace RealEstateAgency.Controllers.RBAC
+{
+
+    public class MenuController// : ModelPagingController<Menu, MenuDto, MenuDto>
+    {
+       
+        public MenuController()
+        {
+        }
+
+    //private Func<IQueryable<Menu>, IQueryable<MenuDto>> _convertor =>
+    //    items => items.Select(i => new MenuDto
+    //    {
+    //        Id = i.Id,
+    //        Name = i.Name,
+    //        ActionName = i.ActionName,
+    //        ControllerName = i.ControllerName,
+    //        ParentId=i.ParentId,
+    //        PluginName=i.PluginName,
+    //        IsPanelPage = i.IsPanelPage
+    //    });
+
+    //public override Func<IQueryable<Menu>, IQueryable<MenuDto>> PagingConverter => _convertor;
+
+    //    public override Func<IQueryable<Menu>, IQueryable<MenuDto>> DtoConverter => _convertor;
+    }
+}
