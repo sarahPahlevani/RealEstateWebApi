cd6ef50d9f10ec093abe2ab9dd0d760582a6eeb7
sara pahlevani <sara.pahlevani@outlook.com>
2020-04-08 02:23:22 +0430

    commit UserGroup Permission

--- RealEstateAgency/Controllers/RBAC/UserGroupPermissionController.cs
+++ RealEstateAgency/Controllers/RBAC/UserGroupPermissionController.cs
@@ -19,12 +19,13 @@ namespace RealEstateAgency.Controllers.RBAC
     {
         
         private readonly IEntityService<RealEstate> _entityService;
-        
+        private readonly IEntityService<UserGroupPermission> _usergrouppermission;
 
-        public UserGroupPermissionController(IModelService<UserGroupPermission, UserGroupPermissionDto> modelService,
+        public UserGroupPermissionController(IModelService<UserGroupPermission, UserGroupPermissionDto> modelService, 
+            IEntityService<UserGroupPermission> usergrouppermission,
             IEntityService<RealEstate> entityService) : base(modelService)
         {
-           
+            _usergrouppermission = usergrouppermission;
             _entityService = entityService;
          }
 
@@ -48,6 +49,23 @@ namespace RealEstateAgency.Controllers.RBAC
             ReadPermission = i.ReadPermission
         });
 
+        [Authorize(Roles = UserGroups.Administrator + "," + UserGroups.RealEstateAdministrator)]
+        [HttpPut("[Action]")]
+        public async Task<ActionResult> UpdateUserGroupPermission([FromBody]UserGroupPermissionDto dto, CancellationToken cancellationToken)
+        {
+            var user = await ModelService.GetAsync(u => u.Id == dto.Id, cancellationToken);
+            user.MenuId = dto.MenuId;
+            user.ReadPermission = dto.ReadPermission;
+           
+            user.UpdatePermission = dto.UpdatePermission;
+            user.ReadPermission = dto.ReadPermission;
+            user.UserGroupId = dto.UserGroupId;
+          
+            await ModelService.UpdateAsync(user, cancellationToken);
+
+            return NoContent();
+        }
+        
         [Authorize(Roles = UserGroups.Administrator + "," + UserGroups.RealEstateAdministrator + "," + UserGroups.Agent)]
         [HttpGet("[Action]")]
         public async Task<ActionResult<IEnumerable<UserGroupPermissionDto>>> GetAllUserGroupPermission(CancellationToken cancellationToken)
@@ -58,9 +76,43 @@ namespace RealEstateAgency.Controllers.RBAC
            MenuId = i.MenuId,
            DeletePermission = i.DeletePermission,
            UpdatePermission = i.UpdatePermission,
-           ReadPermission = i.ReadPermission
+           ReadPermission = i.ReadPermission 
        }).ToListAsync(cancellationToken);
 
+        [Authorize(Roles = UserGroups.Administrator + "," + UserGroups.RealEstateAdministrator + "," + UserGroups.Agent)]
+        [HttpGet("[Action]/{id}")]
+        public async Task<ActionResult<UserGroupPermissionDto>> GetById(int id, CancellationToken cancellationToken)
+      => await ModelService.DbContext.UserGroupPermission.Where(item => item.Id == id).Select(i => new UserGroupPermissionDto
+      {
+          Id = i.Id,
+          UserGroupId = i.UserGroupId,
+          MenuId = i.MenuId,
+          DeletePermission = i.DeletePermission,
+          UpdatePermission = i.UpdatePermission,
+          ReadPermission = i.ReadPermission
+      }).FirstOrDefaultAsync(cancellationToken);
+       
 
+        [Authorize(Roles = UserGroups.Administrator + "," + UserGroups.RealEstateAdministrator + "," + UserGroups.Agent)]
+        [HttpDelete("[Action]/{id}")]
+        public override async Task<ActionResult> Delete(int id, CancellationToken cancellationToken)
+        {
+            return await base.Delete(id, cancellationToken);
+        }
+
+        [Authorize(Roles = UserGroups.Administrator + "," + UserGroups.RealEstateAdministrator + "," + UserGroups.Agent)]
+        [HttpGet("[Action]")]
+        public async Task<ActionResult<IEnumerable<UserPermission>>> GetUserGroupPermission(CancellationToken cancellationToken)
+       => await ModelService.DbContext.UserGroupPermission.Include(item=> item.Menu).Include(item=> item.UserGroup).Select(i => new UserPermission
+       {
+           Id = i.Id,
+           Menu = i.Menu.Name,
+           MenuId = i.MenuId,
+           RoleId=i.UserGroupId,
+           Role=i.UserGroup.Name,
+           HasDeletePermmite = i.DeletePermission,
+           HasUpdatePermmite = i.UpdatePermission,
+           HasReadPermmite = i.ReadPermission
+       }).ToListAsync(cancellationToken);
     }
 }
