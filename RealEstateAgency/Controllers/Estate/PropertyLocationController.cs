5623041e00e0bd22f83a1b0bb4f756d7d21e9178
jafar shahrivar <j.ebadollahi@live.com>
2020-04-23 23:51:56 +0430

    Fix Location country, city

--- RealEstateAgency/Controllers/Estate/PropertyLocationController.cs
+++ RealEstateAgency/Controllers/Estate/PropertyLocationController.cs
@@ -14,11 +14,20 @@ namespace RealEstateAgency.Controllers.Estate
     public class PropertyLocationController : ModelController<PropertyLocation, PropertyLocationDto>
     {
         private readonly IEntityService<Property> _propertyService;
+        private readonly IEntityService<Country> _countryService;
+        private readonly IEntityService<Region> _regionService;
+        private readonly IEntityService<City> _cityService;
 
         public PropertyLocationController(IModelService<PropertyLocation, PropertyLocationDto> modelService
-            , IEntityService<Property> propertyService) : base(modelService)
+            , IEntityService<Property> propertyService
+            , IEntityService<Country> countryService
+            , IEntityService<Region> regionService
+            , IEntityService<City> cityService) : base(modelService)
         {
             _propertyService = propertyService;
+            _countryService = countryService;
+            _regionService = regionService;
+            _cityService = cityService;
         }
 
         public override Func<IQueryable<PropertyLocation>, IQueryable<PropertyLocationDto>> DtoConverter
@@ -46,6 +55,31 @@ namespace RealEstateAgency.Controllers.Estate
         [HttpPost]
         public override async Task<ActionResult<PropertyLocationDto>> Create(PropertyLocationDto value, CancellationToken cancellationToken)
         {
+            var countryItem = _countryService.AsQueryable(r => r.Name == value.Country).FirstOrDefault();
+            if (countryItem == null)
+                countryItem = _countryService.Create(new Country
+                {
+                    CurrencyId = 11,
+                    Name = value.Country,
+                });
+
+            var regionItem = _regionService.AsQueryable(r => r.CountryId == countryItem.Id && r.Name == value.Region).FirstOrDefault();
+            if (regionItem == null)
+                regionItem = _regionService.Create(new Region
+                {
+                    CountryId = countryItem.Id,
+                    Name = value.Region,
+                });
+
+            var cityItem = _cityService.AsQueryable(r => r.RegionId == regionItem.Id && r.Name == value.City).FirstOrDefault();
+            if (cityItem == null)
+                cityItem = _cityService.Create(new City
+                {
+                    RegionId = regionItem.Id,
+                    Name = value.City,
+                });
+            value.CityId = cityItem.Id;
+
             var res = await ModelService.CreateByDtoAsync(value, cancellationToken);
 
             var property = await _propertyService.GetAsync(value.PropertyId, cancellationToken);
@@ -61,6 +95,32 @@ namespace RealEstateAgency.Controllers.Estate
         [HttpPut]
         public override async Task<ActionResult> UpdateAsync(PropertyLocationDto value, CancellationToken cancellationToken)
         {
+            var countryItem = _countryService.AsQueryable(r => r.Name == value.Country).FirstOrDefault();
+            if (countryItem == null)
+                countryItem = _countryService.Create(new Country
+                {
+                    CurrencyId = 11,
+                    Name = value.Country,
+                });
+
+            var regionItem = _regionService.AsQueryable(r => r.CountryId == countryItem.Id && r.Name == value.Region).FirstOrDefault();
+            if (regionItem == null)
+                regionItem = _regionService.Create(new Region
+                {
+                    CountryId = countryItem.Id,
+                    Name = value.Region,
+                });
+
+            var cityItem = _cityService.AsQueryable(r => r.RegionId == regionItem.Id && r.Name == value.City).FirstOrDefault();
+            if (cityItem == null)
+                cityItem = _cityService.Create(new City
+                {
+                    RegionId = regionItem.Id,
+                    Name = value.City,
+                });
+            value.CityId = cityItem.Id;
+
+
             await ModelService.UpdateByDtoAsync(value, cancellationToken);
 
             var property = await _propertyService.GetAsync(value.PropertyId, cancellationToken);
